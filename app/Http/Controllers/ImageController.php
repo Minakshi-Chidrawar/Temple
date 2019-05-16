7912e145ef2627a7686aabe0e8d2fc3f90d8c3ec
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-05-16 19:55:02 +0100

    Controller for images

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use File;
+use Carbon\Carbon;
+
+class ImageController extends Controller
+{
+    public function index()
+    {
+    	return view('image.image-view');
+    }
+
+    public function store(Request $request)
+    {
+        $path = public_path() . '/upload/' . Carbon::now()->isoFormat("DDMMYYYY") . '/';   
+   
+        if(!File::isDirectory($path)){
+            File::makeDirectory($path, 0777, true);
+        }
+
+        $imageName = request()->file->getClientOriginalName();
+        request()->file->move($path, $imageName);
+
+        return response()->json(['uploaded' => $path . $imageName]);
+    }
+}
