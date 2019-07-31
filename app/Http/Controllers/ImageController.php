8f6fdcf66a2edaecfa76772f87dd08601f18f57d
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-07-31 20:13:59 +0100

    Added function for view images

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,28 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use File;
+use App\Image;
+use App\Album;
 use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class ImageController extends Controller
 {
     public function index()
     {
-    	return view('image.image-view');
+        // return view('image.image-view');
+        return view('gallery.home');
     }
 
     public function store(Request $request)
     {
-        $path = public_path() . '/upload/' . Carbon::now()->isoFormat("DDMMYYYY") . '/';   
+        // $path = public_path() . '/upload/' . Carbon::now()->isoFormat("DDMMYYYY") . '/';   
    
-        if(!File::isDirectory($path)){
-            File::makeDirectory($path, 0777, true);
-        }
+        // if(!File::isDirectory($path)){
+        //     File::makeDirectory($path, 0777, true);
+        // }
 
-        $imageName = request()->file->getClientOriginalName();
-        request()->file->move($path, $imageName);
+        // $imageName = request()->file->getClientOriginalName();
+        // request()->file->move($path, $imageName);
 
-        return response()->json(['uploaded' => $path . $imageName]);
+        // return response()->json(['uploaded' => $path . $imageName]);
     }
 }
