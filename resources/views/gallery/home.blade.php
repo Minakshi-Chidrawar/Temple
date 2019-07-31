fba145fb6e2d89a7b53eee690605d60c93321eab
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-07-31 20:49:08 +0100

    Display images on the page

--- resources/views/gallery/home.blade.php
+++ resources/views/gallery/home.blade.php
@@ -8,8 +8,11 @@
 
             <input type="file" name="image" class="form-control">
             <button class="btn btn-primary" type="submit">Submit</button>
-
         </form>
+
+        @foreach($images as $image)
+            <img src="{{ asset('storage/'.$image->name) }}" class="img-thumbnail">
+        @endforeach
     </div>
 </div>
 @endsection
