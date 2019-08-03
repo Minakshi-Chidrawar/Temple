41efa992501ab9579173865fb14b4c9b62bf6087
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-08-03 07:42:34 +0100

    Amended for ref path of the album images

--- resources/views/gallery/gallery.blade.php
+++ resources/views/gallery/gallery.blade.php
@@ -5,10 +5,10 @@
         <div class="row">
             @foreach($gallery as $item)
                 <div class="col-sm-4">
-                    <a href="gallery/ {{ $item->id }}">
+                    <a href="gallery/images/{{ $item->id }}">
                         <div class="item">
                             <img src="storage/uploads/6L8XE46FOlcDhC7Ca8iCqL0f0JacmqZzWOw1Qmuw.jpeg" class="img-thumbnail">
-                            <a href="gallery/ {{ $item->id }}" class="centered">{{ $item->name }}</a>
+                            <a href="gallery/images/{{ $item->id }}" class="centered">{{ $item->name }}</a>
                         </div>
                     </a>
                 </div>
