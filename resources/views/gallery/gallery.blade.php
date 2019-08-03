a5f87a5be7482d74f70943d9173dcf029f69da65
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-08-03 07:18:11 +0100

    View to display gallery

---
+++ resources/views/gallery/gallery.blade.php
@@ -0,0 +1,19 @@
+@extends('master.layout')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            @foreach($gallery as $item)
+                <div class="col-sm-4">
+                    <a href="gallery/ {{ $item->id }}">
+                        <div class="item">
+                            <img src="storage/uploads/6L8XE46FOlcDhC7Ca8iCqL0f0JacmqZzWOw1Qmuw.jpeg" class="img-thumbnail">
+                            <a href="gallery/ {{ $item->id }}" class="centered">{{ $item->name }}</a>
+                        </div>
+                    </a>
+                </div>
+            @endforeach
+
+        </div>
+    </div>
+@endsection
