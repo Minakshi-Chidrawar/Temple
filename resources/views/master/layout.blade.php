17b913c849876aff1dcf5093c076787023d8649f
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-08-06 10:31:02 +0100

    Added script to add/remove buttons for the images

--- resources/views/master/layout.blade.php
+++ resources/views/master/layout.blade.php
@@ -21,8 +21,9 @@
     @include('master.footer')
 
     <!-- Bootstrap core JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-
+    <script src="{{ asset('js/custom.js') }}"></script>
 </body>
 </html>
