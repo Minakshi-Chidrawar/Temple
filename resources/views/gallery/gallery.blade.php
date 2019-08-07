e3b8d92865c02dabac90cd1835e9e8e11d3a2135
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-08-07 09:13:47 +0100

    Amended for the pagination

--- resources/views/gallery/gallery.blade.php
+++ resources/views/gallery/gallery.blade.php
@@ -3,23 +3,25 @@
 @section('content')
     <div class="container">
         <h1>Gallery</h1>
-        <p> <a href="{{ route('album.store') }}">Create Album</a></p>
+        <button class="btn btn-link"> <a href="{{ route('album.store') }}">Create Album</a></button>
         <div class="row">
             @foreach($gallery as $item)
                 <div class="col-sm-4">
                     <a href="gallery/images/{{ $item->id }}">
                         <div class="item">
-                            <a href="gallery/images/{{ $item->id }}" class="centered">{{ $item->name }}</a>
                             @if(empty($item->images[0]))
                                 <img src="images/Mataji.png" class="img-thumbnail">
                             @else
                                 <img src="{{ asset('storage/'.$item->images[0]->name) }}" class="img-thumbnail">
                             @endif
+                            <a href="gallery/images/{{ $item->id }}" class="centered">{{ $item->name }}</a>
                         </div>
                     </a>
                 </div>
             @endforeach
-
+        </div>
+        <div class="panel-heading mt-5 pagination" style="">
+            {{$gallery->links()}}
         </div>
     </div>
 @endsection
