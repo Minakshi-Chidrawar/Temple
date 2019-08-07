a66240ba0c7602360635ae486512528c50ea24e6
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-08-07 07:24:03 +0100

    Added link to create album

--- resources/views/gallery/gallery.blade.php
+++ resources/views/gallery/gallery.blade.php
@@ -2,18 +2,19 @@
 
 @section('content')
     <div class="container">
+        <h1>Gallery</h1>
+        <p> <a href="{{ route('album.store') }}">Create Album</a></p>
         <div class="row">
             @foreach($gallery as $item)
                 <div class="col-sm-4">
                     <a href="gallery/images/{{ $item->id }}">
-
                         <div class="item">
+                            <a href="gallery/images/{{ $item->id }}" class="centered">{{ $item->name }}</a>
                             @if(empty($item->images[0]))
                                 <img src="images/Mataji.png" class="img-thumbnail">
                             @else
                                 <img src="{{ asset('storage/'.$item->images[0]->name) }}" class="img-thumbnail">
                             @endif
-                            <a href="gallery/images/{{ $item->id }}" class="centered">{{ $item->name }}</a>
                         </div>
                     </a>
                 </div>
