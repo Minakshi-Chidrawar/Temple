61c48fc536c7303545ee323c7ffeffa113faa78e
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-08-20 08:58:41 +0100

    Added migration for the vacancy

---
+++ database/migrations/2019_08_20_072535_create_vacancies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVacanciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vacancies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('post')->nullable();
+            $table->text('description');
+            $table->date('postDate');
+            $table->date('closingDate');
+            $table->string('email');
+            $table->smallInteger('mobile');
+            $table->string('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vacancies');
+    }
+}
