316501325fdedc229d551074f1d5285f182eaba3
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-08-20 08:58:08 +0100

    Added model for the vacancy

---
+++ app/Vacancy.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Vacancy extends Model
+{
+    protected $attributes = [
+        'email'  => 'jasmaicha@gmail.com',
+        'mobile' => '07882253540'
+    ];
+}
