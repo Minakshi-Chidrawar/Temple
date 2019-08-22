73f62066ab3abdb0d7ff6090179a3cc2924dc402
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-08-22 23:46:27 +0100

    Added container class

--- resources/views/temple/easyFundRaising.blade.php
+++ resources/views/temple/easyFundRaising.blade.php
@@ -1,12 +1,16 @@
 @extends('master.layout')
 
 @section('content')
-    <h1>Procedure for easyfundraising</h1>
+<div class="back-color">
+    <div class="container pt-4 pb-4">
+        <h1>Procedure for easyfundraising</h1>
 
-    <div>
-        <p style="margin-left: 23%;">Support raising more donation for Mataji’s Temple through easyfundraising.</p>
-            <iframe src="http://wfyw.easyfundraising.org.uk/?id=127729" width="100%" height="2000" frameborder="0" scrolling="no">
-            </iframe>
-        </p>
+        <div>
+            <p style="margin-left: 23%;">Support raising more donation for Mataji’s Temple through easyfundraising.</p>
+                <iframe src="http://wfyw.easyfundraising.org.uk/?id=127729" width="100%" height="2000" frameborder="0" scrolling="no">
+                </iframe>
+            </p>
+        </div>
     </div>
+</div>
 @endsection
