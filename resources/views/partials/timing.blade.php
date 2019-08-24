37439e63e909d9f9e7c7a0a2aa38d22bcc1f336b
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-08-24 21:26:00 +0100

    Separated Timings view

---
+++ resources/views/partials/timing.blade.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <div class="row">
+        <div class="col-6 mt-5">
+            <div class="text-center">
+            <h1>Timings</h1>
+            </div>
+        </div>
+        <div class="col-6 mt-5">
+            <ul class="list-unstyled">
+                <li>Open daily 08:00-18:30 PM</li>
+                <li>Aarti daily at 12:00 noon and 18:00</li>
+                <li class="volunteer">Volunteer needed for Mataji's Temple, please <a href="{{ route('vacancies')}}">click Here</a> for more information.</li>
+            </ul>
+      </div>
+    </div>
+</div>
