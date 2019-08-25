2bdce9214dde180d8419b9a308b3feff065f3306
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-08-25 21:55:52 +0100

    Added event date

--- resources/views/partials/upcomingEvent.blade.php
+++ resources/views/partials/upcomingEvent.blade.php
@@ -1,19 +1,20 @@
 <div class="container">
   <div class="row">
-  <div class="col-6  mt-5">
-      <ul class="list-unstyled">
-        <li>
-          <strong class="font-color">Event Name</strong>: {{ $event->title}}<br>
-          <strong class="font-color">About Event</strong>:<br> {!! $event->description !!}
-        </li>
-        <li>&nbsp;</li>
-        <li class="font-color">You can also find events for {{ date('Y') }} at the temple: <a href="docs/Calendar 2017.pdf">Click Here</a></li>
-      </ul>
-  </div>
-  <div class="col-6" style="margin-top: 10%;">
-    <div class="text-center">
-      <h1>Upcoming Event</h1>
+    <div class="col-6  mt-5">
+        <ul class="list-unstyled">
+          <li>
+            <strong class="font-color">Event Name</strong>: {{ $event->title}}<br>
+            <strong class="font-color">About Event</strong>:<br> {!! $event->description !!}<br>
+            <strong class="font-color">Event is on</strong>: {{ Carbon\Carbon::parse($event->startDate)->isoFormat('MMM Do, YYYY') }}
+          </li>
+          <li>&nbsp;</li>
+          <li class="font-color">You can also find events for {{ date('Y') }} at the temple: <a href="{{ route('calendar') }}">Click Here</a></li>
+        </ul>
     </div>
-  </div>
+    <div class="col-6" style="margin-top: 10%;">
+      <div class="text-center">
+        <h1>Upcoming Event</h1>
+      </div>
+    </div>
   </div>
 </div>
