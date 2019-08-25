d43c9ac4a949869101d505b68a3a96dc3732361f
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-08-25 22:04:29 +0100

    Separated view for the delete modal

--- resources/views/event/events.blade.php
+++ resources/views/event/events.blade.php
@@ -34,40 +34,16 @@
                         @if(Auth::check() && Auth::user()->role == 'admin')
                         <td>
                             <div class="btn btn-group">
-                                <button type="button"  class="btn btn-success btn-sm"><a href="{{ route('event.edit', [$event->id]) }}">Edit</a></button>
+                                <a href="{{ route('event.edit', [$event->id]) }}"><button type="button"  class="btn btn-success btn-sm">Edit</button></a>
                                 <button type="button"  class="btn btn-danger btn-sm" data-toggle="modal" data-target="#exampleModal{{ $event->id }}">Delete</button>
                             </div>
                         </td>
                         @endif
                     </tr>
+                    @include('event.deleteModal')
                 @endforeach
             </table>
 
-            <!-- Delete Modal -->
-            <div class="modal fade" id="exampleModal{{ $event->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Delete</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            Do you want to delete?
-                        </div>
-                        <div class="modal-footer">
-                            <form action="{{ route('event.delete') }}" method="POST">
-                                @csrf
-                                <input type="hidden" name="id" value="{{ $event->id }}">
-                                <button class="btn btn-danger" type="submit">Delete</button>
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            </form> 
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- End of the Delete Modal -->
             <div class="panel-heading mt-5 pagination" style="">
                 {{$events->links()}}
             </div>
