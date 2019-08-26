1497bd5b000d64ee361e661c874ae7ba8e3d20fd
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-08-26 21:31:05 +0100

    Show the timings from the Content table

--- resources/views/partials/timing.blade.php
+++ resources/views/partials/timing.blade.php
@@ -5,10 +5,10 @@
             <h1>Timings</h1>
             </div>
         </div>
-        <div class="col-6 mt-5">
+        <div class="col-6 mt-3">
+            {!! $content->description !!}
+            <br>
             <ul class="list-unstyled">
-                <li>Open daily 08:00-18:30 PM</li>
-                <li>Aarti daily at 12:00 noon and 18:00</li>
                 <li class="volunteer">Volunteer needed for Mataji's Temple, please <a href="{{ route('vacancies')}}">click Here</a> for more information.</li>
             </ul>
       </div>
