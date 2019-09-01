3b53d7b9a4c410153d0b1ed376e7f0fbbd14454c
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-09-01 20:48:15 +0100

    Added vue component for the dropzone

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,27 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
+        <div class="row justify-content-center mb-3" ref="imageUpload">
+            <div class="col-12 bg-dark text-white rounded my-2 py-3 text-center">
+                <h2>DROP IMAGE(S) HERE</h2>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import Dropzone from 'dropzone';
+
     export default {
+        data: function() {
+            return {
+                dropzone: null
+            }
+        },
+
         mounted() {
-            console.log('Component mounted.')
+            this.dropzone = new Dropzone(this.$refs.imageUpload, {
+                url: '/api/homeImages'
+            });
         }
     }
 </script>
