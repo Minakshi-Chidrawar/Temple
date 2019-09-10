dc673c7c886a02cb2699f9a6ccd7c30e8ef6bbbf
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-09-10 22:49:47 +0100

    Amended for the thumbnail column

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -75,24 +75,28 @@ class ImageController extends Controller
     }
 
     public function uploadAndSaveImages($request, $albumId)
-    {        
+    {      
         if($request->hasFile('image'))
         {
             foreach ($request->file('image') as $image)
             {
-                $imageName = rand(1111,9999).time() . '.' . $image->getClientOriginalExtension();
-                $path = 'upload/' . $imageName;
-                $img = IntervensionImage::make($image);
-                $img->fit(250, 250, function ($constraint) {
-                    $constraint->upsize();
-                });
-                $img->save($path);
+                $basename = rand(1111,9999).time();
+                $original = $basename . '.' . $image->getClientOriginalExtension();
+                $thumbnail = $basename . '_thumb.' . $image->getClientOriginalExtension();
+                $thumbnail_path = 'upload/' . $thumbnail;
+
+                $thumbnail_img = IntervensionImage::make($image)
+                                        ->fit(250, 250)
+                                        ->save($thumbnail_path);
+
+                $image->move(public_path('upload/'), $original);
 
                 Image::create([
-                    'name' => $path,
+                    'original' => 'upload/' . $original,
+                    'thumbnail' => $thumbnail_path,
                     'album_id' => $albumId
                 ]);
-            }
+            }	
         }
     }
 }
