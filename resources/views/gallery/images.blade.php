77722d3e8a12a21b0d96850b3ba83f9a25b5fb0b
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-09-11 19:41:19 +0100

    Included deletemodal

--- resources/views/gallery/images.blade.php
+++ resources/views/gallery/images.blade.php
@@ -29,7 +29,7 @@
             @foreach($gallery->images as $image)
                 <div class="col-sm-4">
                     <div class="item">
-                        <img src="{{ asset('/'.$image->name) }}" class="img-thumbnail">
+                        <img src="{{ asset('/'.$image->thumbnail) }}" class="img-thumbnail">
                     </div>
 
                     <!-- Button trigger modal -->
@@ -39,31 +39,7 @@
                         </button>
                     @endif
                     <!-- Delete Modal -->
-                    <div class="modal fade" id="exampleModal{{ $image->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Delete</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    Do you want to delete the image 
-                                    <img src="{{ asset('/'.$image->name) }}" alt="$image->name" style="width: 50px; height: 50px;">?
-                                </div>
-                                <div class="modal-footer">
-                                    <form action="{{ route('image.delete', $image->id) }}" method="POST">
-                                        @method('DELETE')
-                                        @csrf
-                                        <input type="hidden" name="id" value="{{ $image->id }}">
-                                        <button class="btn btn-danger" type="submit">Delete</button>
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                    </form> 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    @include('gallery.deleteModal')
                     <!-- End of the Delete Modal -->
                 </div>
             @endforeach
