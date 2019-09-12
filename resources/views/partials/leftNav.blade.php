4866ae1fa92a0ecbe6c3369cec96640d4a7208b1
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-09-12 22:15:31 +0100

    Refactored

---
+++ resources/views/partials/leftNav.blade.php
@@ -0,0 +1,23 @@
+<ul class="navbar-nav mr-auto">
+    <li class="nav-item active">
+        <a class="nav-link" href="{{route('main')}}">Home <span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{route('aarati')}}">Aartis</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{route('inTempleActivities')}}">Priest Services</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{route('events')}}">News & Events</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{route('gallery')}}">Gallery</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{route('mission')}}">Mission</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{route('donation')}}">Donations</a>
+    </li>
+</ul>
