2cdbb3419a6e48ecfd464f420339f9c733f368f7
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-09-12 22:18:51 +0100

    Refactored

--- resources/views/homeUpload/create.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('master.layout')
-
-@section('content')
-    <div class="flex-center position-ref full-height" id="app">
-        <example-component></example-component>
-    </div>
-
-    <script src="{{ mix('/js/app.js') }}"></script>
-@endsection
