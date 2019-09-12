1c9bdc4c1c7f237de3da19b4720accf8279915f3
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-09-12 22:15:17 +0100

    Refactored

---
+++ resources/views/partials/headLink.blade.php
@@ -0,0 +1,25 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<title>@yield('title', 'Shree Adhya Shakti Mataji')</title>
+
+<link href="https://fonts.googleapis.com/css?family=Cardo|Vollkorn:400i&display=swap" rel="stylesheet">
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+<link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+
+<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('js/custom.js') }}"></script>
+
+<!-- include summernote css/js -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-bs4.css" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-bs4.js"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.summernote').summernote({height: 300 });
+    });
+</script>
