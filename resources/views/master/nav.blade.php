f2ea4fc1ae02a2f742ed6b804d4546b5a2a99537
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-09-12 22:17:58 +0100

    Refactored

--- resources/views/master/nav.blade.php
+++ resources/views/master/nav.blade.php
@@ -1,32 +1,10 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark nav-dark">
     <a class="navbar-brand" href="{{route('main')}}">Shree Adhya Shakti Mataji</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
         <!-- Left Side Of Navbar -->
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="{{route('main')}}">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{route('aarati')}}">Aartis</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{route('inTempleActivities')}}">Priest Services</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{route('events')}}">News & Events</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{route('gallery')}}">Gallery</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{route('mission')}}">Mission</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{route('donation')}}">Donations</a>
-            </li>
-        </ul>
+        @include('partials.leftNav')
     </div>
 </nav>
