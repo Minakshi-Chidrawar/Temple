56a5df5b79e4e6e7a153086c580614df1142a186
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-09-12 22:16:24 +0100

    Refactored

--- resources/views/partials/timing.blade.php
+++ resources/views/partials/timing.blade.php
@@ -6,10 +6,9 @@
             </div>
         </div>
         <div class="col-6 mt-3">
-            @if(!(empty($content)))
             {!! $content->description !!}
             <br>
-            @endif
+
             <ul class="list-unstyled">
                 <li class="volunteer">Volunteer needed for Mataji's Temple, please <a href="{{ route('vacancies')}}">click Here</a> for more information.</li>
             </ul>
