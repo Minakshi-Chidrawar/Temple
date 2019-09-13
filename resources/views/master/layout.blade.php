dffa79ec1d6e3b00b53be9bd6e8fbafffc3edacc
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-09-13 21:42:20 +0100

    Refactored

--- resources/views/master/layout.blade.php
+++ resources/views/master/layout.blade.php
@@ -1,28 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title', 'Shree Adhya Shakti Mataji')</title>
-    <link href="https://fonts.googleapis.com/css?family=Cardo|Vollkorn:400i&display=swap" rel="stylesheet">
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
-
-    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('js/custom.js') }}"></script>
-
-    
-    <!-- include summernote css/js -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-bs4.css" rel="stylesheet">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-bs4.js"></script>
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $('.summernote').summernote({height: 300 });
-        });
-    </script>
-
+    @include('partials.headLink')
 </head>
 
 <body>
@@ -34,9 +13,5 @@
     <!-- /div -->
 
     @include('master.footer')
-
-    <!-- Bootstrap core JavaScript -->
-    <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> -->
-    <!-- <script src="{{ mix('/js/app.js') }}"></script> -->
 </body>
 </html>
