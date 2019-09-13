99d03e40c81f7bdf13f86c6765003dda07f93dfc
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-09-13 21:43:42 +0100

    Amended to include the advert images

--- resources/views/event/events.blade.php
+++ resources/views/event/events.blade.php
@@ -9,44 +9,51 @@
         @endif
 
         @if(Auth::check() && Auth::user()->role == 'admin')
-        <div class="row mb-4 mt-2">
-            <button class="btn btn-success"> <a href="{{ route('event.create') }}">
-                Add Event</a>
-            </button>
-        </div>
-        @endif
-
-        @if(!empty($events))
-            <table class="table">
-                <thead>
-                    <th>Event Title</th>
-                    <th>Event Start Date</th>
-                    <th>Event End Date</th>
-                    <th>Sponsored By</th>
-                    <th></th>
-                </thead>
-                @foreach($events as $event)
-                    <tr>
-                        <td><a href="events/event/{{ $event->id }}">{{ $event->title }}</a></td>
-                        <td>{{ $event->startDate }}</td>
-                        <td>{{ $event->endDate }}</td>
-                        <td>{{ $event->sponsor }}</td>
-                        @if(Auth::check() && Auth::user()->role == 'admin')
-                        <td>
-                            <div class="btn btn-group">
-                                <a href="{{ route('event.edit', [$event->id]) }}"><button type="button"  class="btn btn-success btn-sm">Edit</button></a>
-                                <button type="button"  class="btn btn-danger btn-sm" data-toggle="modal" data-target="#exampleModal{{ $event->id }}">Delete</button>
-                            </div>
-                        </td>
-                        @endif
-                    </tr>
-                    @include('event.deleteModal')
-                @endforeach
-            </table>
-
-            <div class="panel-heading mt-5 pagination" style="">
-                {{$events->links()}}
+            <div class="row mb-4 mt-2">
+                <button class="btn btn-success"> <a href="{{ route('event.create') }}">
+                    Add Event</a>
+                </button>
             </div>
         @endif
+
+        <div class="row">
+            <div class="col-9">
+                @if(count($events) > 0)
+                    <table class="table">
+                        <thead>
+                            <th>Event Title</th>
+                            <th>Event Start Date</th>
+                            <th>Event End Date</th>
+                            <th>Sponsored By</th>
+                            <th></th>
+                        </thead>
+                        @foreach($events as $event)
+                            <tr>
+                                <td><a href="events/event/{{ $event->id }}">{{ $event->title }}</a></td>
+                                <td>{{ $event->startDate }}</td>
+                                <td>{{ $event->endDate }}</td>
+                                <td>{{ $event->sponsor }}</td>
+                                @if(Auth::check() && Auth::user()->role == 'admin')
+                                <td>
+                                    <div class="btn btn-group">
+                                        <a href="{{ route('event.edit', [$event->id]) }}"><button type="button"  class="btn btn-success btn-sm">Edit</button></a>
+                                        <button type="button"  class="btn btn-danger btn-sm" data-toggle="modal" data-target="#exampleModal{{ $event->id }}">Delete</button>
+                                    </div>
+                                </td>
+                                @endif
+                            </tr>
+                            @include('event.deleteModal')
+                        @endforeach
+                    </table>
+
+                    <div class="panel-heading mt-5 pagination" style="">
+                        {{$events->links()}}
+                    </div>
+                @endif
+            </div>
+            <div class="col-2">
+                @include('partials.advert')
+            </div>
+        </div>
     </div>
 @endsection
