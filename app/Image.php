234b4182b6dae5a821819ae080f0cba530aff99b
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-10-25 22:47:57 +0100

    Added belongs to

--- app/Image.php
+++ app/Image.php
@@ -2,9 +2,14 @@
 
 namespace App;
 
+use App\Album;
 use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
     protected $fillable = ['original', 'thumbnail', 'album_id'];
+
+    public function albums(){
+        return $this->belongsTo(Album::class);
+    }
 }
